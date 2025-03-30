9a43959056b2b1c35592e89504504084f3ca72e4
Kitson Kelly <me@kitsonkelly.com>
2025-03-31 09:38:18 +1100

    scaffold out index

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,8 +5,10 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>bayside-bricks</title>
+        <title>Bayside Brickstore</title>
         <link rel="stylesheet" href="/styles.css" />
+        <script src="//cache.bricklink.info/vendor/iframeResizer.contentWindow.3.5.3.min.js">
+        </script>
       </head>
       <body>
         <Component />
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,73 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
+    <div class="relative text-right w-full">
+      <img
+        src="https:////www.bricklink.com/myImg/1035859.JPG"
+        alt="Bayside Brickstore Logo"
+        class="h-96 mt-2 mr-2 ml-auto"
+      />
+      <main class="absolute text-left top-4 left-4">
+        <h1 class="text-5xl font-bold my-4">Welcome to Bayside Brickstore</h1>
+        <h2 class="text-4xl my-3">
+          Victoria's new kid on the block, selling a wide selection of
+        </h2>
+        <ul class="ml-8 space-y-2">
+          <li class="flex items-center">
+            <img
+              class="mr-4"
+              src="https://www.bricklink.com/myImg/1035787.JPG"
+            />Complete, used sets; many with original packaging
+          </li>
+          <li class="flex items-center">
+            <img
+              class="mr-4"
+              src="https://www.bricklink.com/myImg/1035788.JPG"
+            />New and used minifigures
+          </li>
+          <li class="flex items-center">
+            <img
+              class="mr-4"
+              src="https://www.bricklink.com/myImg/1035789.JPG"
+            />Rare bricks and other parts, with new items added daily
+          </li>
+        </ul>
+        <h2 class="text-4xl my-3">
+          Quality, Honesty, Attention to detail
+        </h2>
+        <ul class="ml-8 space-y-2">
+          <li class="flex items-center">
+            <img
+              class="mr-4"
+              src="https://www.bricklink.com/myImg/1035790.JPG"
+            />
+            Every item, every brick, every minifigure that we sell has been
+            individually checked, and double-checked to ensure you know what you
+            are buying.
+          </li>
+          <li class="flex items-center">
+            <img
+              class="mr-4"
+              src="https://www.bricklink.com/myImg/1035791.JPG"
+            />
+            Most used items are sold in near-new condition. Where this is not
+            the case, we aim to describe imperfections so you can make an
+            informed choice. Unlike other stores, "used" doesn't leave you
+            hoping for the best.
+          </li>
+          <li class="flex items-center">
+            <img
+              class="mr-4"
+              src="https://www.bricklink.com/myImg/1035792.JPG"
+            />
+            Instant checkouts and fast shipping anywhere across Australia or
+            internationally, with discounted or free shipping for larger orders.
+          </li>
+        </ul>
+        <div class="mt-8 italic text-2xl text-center">
+          We'll be busy adding inventory over the coming weeks, so come back
+          regularly to discover your next build.
+        </div>
+      </main>
     </div>
   );
 }
