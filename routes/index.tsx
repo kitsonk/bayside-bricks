b730a8e0aa28c1f5efa880b9ed6c2cc9392e4883
Ludeck <Ludeck1000@hotmail.com>
2025-03-30 23:17:10 +0000

    spacing

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         class="h-96 mt-2 mr-2 ml-auto"
       />
       <main class="absolute text-left top-4 left-4">
-        <h1 class="text-5xl font-bold mt-4 mb-6">Welcome to Bayside Brickstore</h1>
+        <h1 class="text-5xl font-bold mt-4 mb-8">Welcome to Bayside Brickstore</h1>
         <h2 class="text-3xl my-4 space-y-1">
           Victoria's new kid on the block, selling a wide selection of...
         </h2>
